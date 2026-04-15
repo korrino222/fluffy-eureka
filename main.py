f04f02dd87fe23b750195f3a474436a532678014
ishish.io <ts@ishish.io>
2026-04-15 20:56:50 +0200

    Adding amazing feature

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-token = "ghp_abcdefghijklmnopqrstuvwxyz123456"
-OPENAI_API_KEY="sk-proj-A1B2C3D4E5F6G7H8I9J0K1L2M3N4O5P6Q7R8S9T0U1V2W3X4Y5Z6"
+token = "ghp_4bcde6ghijklmnopqrst8vwxyz123456"
+OPENAI_API_KEY="sk-proj-81B2C3D4E5F6G7H8I9J0K1L2M3N4O5P6Q7R8S9T0U1V2W3X4Y5Z6"
 
 
 @app.get("/hello")
